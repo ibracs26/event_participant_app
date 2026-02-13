ae07ab8a261df742ead11f87fa9f9d6b820fec84
Ibrahim KONDOMBO <kondomboibrahim08@gmail.com>
2026-02-13 17:48:21 +0100

    Create splash screen implementation in splash_page.dart

---
+++ lib/presentation/pages/auth/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to login page after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[ 
+            Image.asset('assets/logo.png'),  // Ensure you have the logo in your assets
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
